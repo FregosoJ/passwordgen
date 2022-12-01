e22f85aef6e0ebc83b4eb98ed62c593dba4569a9
Jonathan Fregoso <jonathanfregoso@Jonathans-MBP.lan>
2022-11-30 22:48:43 -0800

    new commit

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,25 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword(){
 var lowerArr= ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 var upperArr= ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 var numberArr= ['0','1','2','3','4','5','6','7','8','9']
 var symbolArr= ['!','@','#','$','%','&','*','-','+']
-var num = '';
-var passwoedArray=[];
+var input = [];
+var passwordArray=[];
 
-function generatePassword(){
+
 // Lenth of password
+function generatePassword(){
 var length= prompt("How many charectors would you like your password to be.");
 length = parseInt(length);
 
 if (length<9 || length>128){
-  alert("password must be between 9-100 charactors");
+  alert("password must be between 9-128 charactors");
   return generatePassword();
 }
-// console.log(length);
+console.log(length);
 
 
 // Generate ? for the password
@@ -25,60 +27,59 @@ var low = confirm("Would you like lower case letters")
 var upp = confirm("Would you like uppercase letters")
 var symbols = confirm("Would you like special charactos")
 var numero= confirm("Would you like to include numbers")
+var minimumCount = 0;
 
-if (!lowerArr && !upperArr && !numberArr &&!symbolArr){
+// if statements 
+if (!low && !upp && !symbols && !numero){
   alert("You need to selectone type of charactor");
   return generatePassword()
 }
-
-
-// use if statements for y/n
-if (length == true){
-var userLower = (window.confirm("LowerCase? (V/N)"));
-var userUpper = (window.confirm("UpperCase? (Y/N)"));
-var userNumeric =(window.confirm("Numeric? (Y/N)"));
-var userSpecial= (window .confirm("Special? (Y/N)"));
-}
-
-
+// concat [] together into the empty input []
 if (low){
-  num = num.concat(lowerArr)
-  // console.log(num)
+  input = input.concat(lowerArr)
+  console.log(input)
 }
 
 if (upp){
-  num = num.concat(upperArr)
+  input = input.concat(upperArr)
 }
 
 if (symbols){
-  num = num.concat(symbolArr)
+  input = input.concat(symbolArr)
 }
 
 if (numero){
-  num= num.concat(numberArr)
+  input= input.concat(numberArr)
 }
 
-console.log(num);
+console.log(input)
 
+console.log(typeof input)
+
+
+for (let i = 0; i < length; i++) {
+ index = Math.random() * input.length;
+ passwordArray += input[index];
 }
+return password.join("")
 
 function genRandomNum(){
-num = math.random() *1000
-}
-
-function printRandomNum(){
-generateBtn
-}
-
+  index = Math.floor(Math.random()) * input.length
+  passwordArray += input[index.length]
+  input = math.random() *100;
+  }
+// console.log("passwordArray")
+console.log(passwordArray)
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
+function writePassword(){
+var password = generatePassword();
+var passwordText = document.querySelector("#password");
+}
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+}
